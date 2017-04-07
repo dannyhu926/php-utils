b06924d75c7084302038cea5f06567824f1c6f15
dany <aa@com>
2017-04-07 15:02:43 +0800

    no message

--- tests/StringTest.php
+++ tests/StringTest.php
@@ -15,8 +15,8 @@
 
 namespace JBZoo\PHPUnit;
 
-use JBZoo\Utils\Str;
-use JBZoo\Utils\Slug;
+use Utils\Str;
+use Utils\Slug;
 
 /**
  * Class StringTest
@@ -77,20 +77,11 @@ class StringTest extends PHPUnit
         is('One &amp; Two &lt;&gt; &mdash;', Str::htmlEnt('One &amp; Two <> &mdash;', true));
     }
 
-    public function testUnique()
-    {
-        is(15, strlen(Str::unique()));
-        is(8, strlen(Str::unique(null)));
-        is(10, strlen(Str::unique('t-')));
-        isNotSame(Str::unique(), Str::unique());
-    }
 
     public function testRandom()
     {
         is(10, strlen(Str::random()));
         is(10, strlen(Str::random(10)));
-        is(10, strlen(Str::random(10, true)));
-        is(10, strlen(Str::random(10, false)));
 
         isNotSame(Str::random(), Str::random());
         isNotSame(Str::random(), Str::random());
@@ -103,34 +94,6 @@ class StringTest extends PHPUnit
         is('341', Str::zeroPad(341, 3));
         is('0341', Str::zeroPad(341, 4));
         is('000341', Str::zeroPad(341, 6));
-    }
-
-    public function testTruncateSafe()
-    {
-        is('The quick brown fox...', Str::truncateSafe('The quick brown fox jumps over the lazy dog', 24));
-        is('The quick brown fox jumps over the lazy dog', Str::truncateSafe('The quick brown fox jumps over the lazy dog', 55));
-        is('Th...', Str::truncateSafe('The quick brown fox jumps over the lazy dog', 2));
-        is('The...', Str::truncateSafe('The quick brown fox jumps over the lazy dog', 3));
-        is('The...', Str::truncateSafe('The quick brown fox jumps over the lazy dog', 7));
-    }
-
-    public function testLimitChars()
-    {
-        is('The quick brown fox jump...', Str::limitChars('The quick brown fox jumps over the lazy dog', 24));
-        is('The quick brown fox jumps over the lazy dog', Str::limitChars('The quick brown fox jumps over the lazy dog', 55));
-        is('Th...', Str::limitChars('The quick brown fox jumps over the lazy dog', 2));
-        is('The...', Str::limitChars('The quick brown fox jumps over the lazy dog', 3));
-        is('The qui...', Str::limitChars('The quick brown fox jumps over the lazy dog', 7));
-        is('The quick brown fox jumps over the lazy dog', Str::limitChars('The quick brown fox jumps over the lazy dog', 150));
-    }
-
-    public function testLimitWords()
-    {
-        is('The quick brown...', Str::limitWords('The quick brown fox jumps over the lazy dog', 3));
-        is('The quick brown fox jumps...', Str::limitWords('The quick brown fox jumps over the lazy dog', 5));
-        is('The...', Str::limitWords('The quick brown fox jumps over the lazy dog', 1));
-        is('The quick brown fox jumps over the lazy dog', Str::limitWords('The quick brown fox jumps over the lazy dog', 90));
-        is('The quick brown fox jumps over the...', Str::limitWords('The quick brown fox jumps over the lazy dog', 7));
     }
 
     public function testLike()
@@ -170,42 +133,6 @@ class StringTest extends PHPUnit
         is('one231251251', Slug::filter('123----1251251', '', true));
     }
 
-    public function testMBString()
-    {
-        isSame(Str::isMBString(), function_exists('mb_strtoupper'));
-        isSame(Str::isOverload(), ((int)ini_get('mbstring.func_overload') & MB_OVERLOAD_STRING));
-
-        is(5, Str::len('Денис'));
-
-        isSame(1, Str::pos('Денис', 'е'));
-        isSame(false, Str::pos('Денис', 'Е'));
-        isSame(3, Str::rpos('Денис', 'и'));
-        isSame(1, Str::ipos('Денис', 'Е'));
-        isSame(1, Str::ipos('Денис', 'Е'));
-
-        isSame('енис', Str::strstr('Денис', 'е'));
-        isSame('енис', Str::istr('Денис', 'Е'));
-
-        isSame('ис', Str::rchr('Денис', 'и'));
-
-        isSame('нис', Str::sub('Денис', 2));
-        isSame('ени', Str::sub('Денис', 1, 3));
-
-        isSame('денис', Str::low('ДЕНИС'));
-        isSame('ДЕНИС', Str::up('денис'));
-
-        isSame(2, Str::subCount('денис ДеНИС', 'е'));
-        isSame(1, Str::subCount('денис ДеНИС', 'И'));
-
-        isTrue(Str::isStart('денис', 'ден', true));
-        isTrue(Str::isStart('денис', 'ДЕН', false));
-        isFalse(Str::isStart('денис', 'ДЕН', true));
-
-        isTrue(Str::isEnd('денис', 'нис', true));
-        isTrue(Str::isEnd('денис', 'НИС', false));
-        isFalse(Str::isEnd('денис', 'ДЕНИС', true));
-    }
-
     public function testEsc()
     {
         isSame(
@@ -224,57 +151,11 @@ class StringTest extends PHPUnit
 
     public function testSplitCamelCase()
     {
-        isSame('_', Str::splitCamelCase('_'));
-        isSame('word', Str::splitCamelCase('word'));
-        isSame('word_and_word', Str::splitCamelCase('wordAndWord'));
-        isSame('word_123_number', Str::splitCamelCase('word123Number'));
-        isSame('word number', Str::splitCamelCase('wordNumber', ' '));
-        isSame('word Number', Str::splitCamelCase('wordNumber', ' ', false));
-        isSame('word_Number', Str::splitCamelCase('wordNumber', '_', false));
-        isSame('Word_Number', Str::splitCamelCase('WordNumber', '_', false));
-    }
-
-    public function testTestName2Human()
-    {
-        isSame('test', Str::testName2Human('test'));
-        isSame('testTest', Str::testName2Human('testTest'));
-        isSame('test_Test', Str::testName2Human('test_Test'));
-        isSame('test_test', Str::testName2Human('test_test'));
-        isSame('test test', Str::testName2Human('test test'));
-        isSame('test Test', Str::testName2Human('test Test'));
-
-        isSame('Function', Str::testName2Human('testFunctionTest'));
-        isSame('Function', Str::testName2Human('testFunction_Test'));
-        isSame('Function', Str::testName2Human('Function_Test'));
-
-        isSame('Function Trim', Str::testName2Human('FunctionTrim_Test'));
-        isSame('Function Trim', Str::testName2Human('Function_Trim_Test'));
-        isSame('Function Trim', Str::testName2Human('Function_ Trim _Test'));
-        isSame('Function Trim', Str::testName2Human('Function _ Trim_ Test'));
-        isSame('Function Trim', Str::testName2Human('Function _ trim_ Test'));
-        isSame('Function Trim', Str::testName2Human('Function _Trim_ Test'));
-        isSame('Function Trim', Str::testName2Human('Function_trim_Test'));
-        isSame('Function Trim', Str::testName2Human('Function _trim_ Test'));
-        isSame('Function Trim', Str::testName2Human('Function_ trim _Test'));
-        isSame('Function Trim', Str::testName2Human('Function _ trim _ Test'));
-        isSame('Function Trim', Str::testName2Human('testFunction _ trim _ Test'));
-        isSame('Function Trim', Str::testName2Human('testfunction _ trim _ Test'));
-        isSame('Function Trim', Str::testName2Human('TestFunction _ trim _ Test'));
-        isSame('Function Trim', Str::testName2Human('Test_Function _ trim _ Test'));
-        isSame('Function Trim', Str::testName2Human('Test_ Function _ trim _ Test'));
-        isSame('Function Trim', Str::testName2Human('Test _ Function _ trim _ Test'));
-        isSame('Function Trim', Str::testName2Human('Test_ Function _ trim _ Test'));
-        isSame('Function Test', Str::testName2Human('Test_Function_test_Test'));
-        isSame('Function Test', Str::testName2Human('Test_Function_Test_Test'));
-        isSame('Function JQuery', Str::testName2Human('Test_FunctionJQuery_Test'));
-        isSame('Function IE', Str::testName2Human('Test_FunctionIE_Test'));
-        isSame('Function IE Test', Str::testName2Human('Test_FunctionIE_TestTest'));
-        isSame('Test Function IE Test', Str::testName2Human('Test_testFunctionIE_TestTest'));
-
-        isSame('Function IE', Str::testName2Human('\\JBZoo\\Test_FunctionIE_Test'));
-        isSame('Function IE', Str::testName2Human('\\JBZoo\\PHPHunit\\Test_FunctionIE_Test'));
-        isSame('Function IE', Str::testName2Human('\\JBZoo\\PHPHunit\\Some\\Test_FunctionIE_Test'));
-        isSame('Function IE', Str::testName2Human('\\JBZoo\\PHPHunit\\Some\\Some\\Test_FunctionIE_Test'));
+        isSame('_', Str::snake('_'));
+        isSame('word', Str::snake('word'));
+        isSame('word_and_word', Str::snake('wordAndWord'));
+        isSame('word_123_number', Str::snake('word123Number'));
+        isSame('word number', Str::snake('wordNumber', ' '));
     }
 
     public function testGenerateUUID()
