55a2fec1fe659c4930b45adce760d6174d203e5c
hudeyong <hudeyong@icsoc.net>
2020-08-19 10:28:11 +0800

    导出并压缩

--- Utils/Excel.php
+++ Utils/Excel.php
@@ -1,207 +1,91 @@
 <?php
+/**
+ *      $service->generatedFile($fileName, 0);
+ *      $outputFilename = $service->getZip('开票详情'.date('Ymd').".zip");
+ *      $filePath = '/data/www/html/csv/'.basename($outputFilename);
+ *      $this->download($filePath);
+ */
 
 namespace Utils;
 
-/**
- * @author        dannyhu
- * Excel          导出导入数据类
- * $excelUtils =  Excel::getInstance();
- * $objPHPExcel = $excelUtils->getExcel();
- */
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+use PhpOffice\PhpSpreadsheet\Worksheet\MemoryDrawing;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 class Excel
 {
     const PASSWORD = 'admin';
-
-    private static $_instanceObj = null;
     private $excelObj = null;
 
-    public function __construct()
-    {
-        /*导入phpExcel核心类  */
-        include_once('Excel/PHPExcel.php');
-        include_once('Excel/PHPExcel/Writer/Excel2007.php'); //用于其他低版本xls
-        include_once('Excel/PHPExcel/Writer/Excel5.php'); //用于2007格式
-        include_once('Excel/PHPExcel/IOFactory.php');
-    }
-
-    public static function getInstance()
-    {
-        if (!(self::$_instanceObj instanceof self) || self::$_instanceObj == null) {
-            self::$_instanceObj = new self();
-        }
-        return self::$_instanceObj;
-    }
+    private $_fileName = null; //文件名
+    private $_fileList = []; //文件列表
+    private $_path = ''; //路径
 
     public function getExcel()
     {
-        $this->excelObj = new PHPExcel();
+        $this->excelObj = new Spreadsheet();
+
         return $this->excelObj;
-    }
-
-    /**
-     * 生成Excel文件
-     * param $outputFileName Excel文件名
-     * param $outputExplorer 浏览器输出或文件输出
-     */
-    public function generatedFile($outputFileName, $outputExplorer = 1)
-    {
-        $obj = $this->excelObj;
-        $obj->setActiveSheetIndex(0);
-        //页眉页脚
-        $obj->getActiveSheet()->getHeaderFooter()->setOddHeader('&L&BPersonal cash register&RPrinted on &D');
-        $obj->getActiveSheet()->getHeaderFooter()->setOddFooter('&L&B'.$obj->getProperties()->getTitle().'&RPage &P of &N');
-
-        // 设置页方向和规模
-        $obj->getActiveSheet()->getPageSetup()->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_PORTRAIT);
-        $obj->getActiveSheet()->getPageSetup()->setPaperSize(PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4);
-
-        $excel_type = PHPExcel_IOFactory::identify($outputFileName);
-        $objWriter = PHPExcel_IOFactory::createWriter($obj, $excel_type);
-        if ($outputExplorer > 0) { //输出内容到浏览器
-            header('Content-Type: application/vnd.ms-excel;charset=UTF-8');
-            header("Cache-Control:must-revalidate,post-check=0,pre-check=0");
-            header("Pragma:no-cache");
-            header("Content-Disposition:inline;filename={$outputFileName}");
-            $objWriter->save('php://output');
-            $obj->disconnectWorksheets();
-            unset($obj);
-            exit;
-        } else { //输出内容到文件通过文件路径再用Ajax无刷新页面
-            $objWriter->save("{$outputFileName}");
-            unset($obj);
-        }
-    }
-
-    /**
-     * 读取Excel的数据
-     * @param        $callback   对数据进行操作的回调方法名
-     * @param string $column_arr 文件路径 eg:['id', 'phone', 'province', 'city', 'operators', 'area_code', 'post_code'];
-     * @param        $filename   读取excel路径文件名
-     */
-    public function readData($filename, $column_arr, $sheetIndex = 1)
-    {
-        $reader_type = PHPExcel_IOFactory::identify($filename);
-        $objReader = PHPExcel_IOFactory::createReader($reader_type);
-        if ($reader_type == "CSV") {
-            $objReader->setInputEncoding('GBK');
-        }
-        $objReader->setReadDataOnly(true); //只读取数据，忽略里面各种格式等(对于Excel读去，有很大优化)
-        $objPHPExcel = $objReader->load($filename);
-        $sheetIdx = max((int) $sheetIndex - 1, 0);
-        $objWorksheet = $objPHPExcel->getSheet($sheetIdx);
-
-        $highestRow = $objWorksheet->getHighestRow(); // 取得总行数
-        $highestColumn = $objWorksheet->getHighestColumn(); // 取得总列数
-        $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
-        if ($highestColumnIndex != count($column_arr)) {
-            throw new Exception('column_arr参数配置错误');
-        }
-        $excelData = array();
-        for ($row = 1; $row <= $highestRow; $row++) {
-            for ($col = 0; $col < $highestColumnIndex; $col++) {
-                $excelData[$row][$column_arr[$col]] = (string) $objWorksheet->getCellByColumnAndRow($col, $row)->getValue();
-            }
-            if (implode('', $excelData[$row]) == '') {
-                unset($excelData[$row]);
-            }
-        }
-        return $excelData;
-    }
-
-    /**
-     * 读取excel 指定的行数和列数 转换成数组
-     *
-     * @param string $excelFile  文件路径
-     * @param string $column_arr 文件路径 eg:['id', 'phone', 'province', 'city', 'operators', 'area_code', 'post_code'];
-     * @param int    $startRow   开始读取的行数
-     * @param int    $chunkSize  读取的条数
-     * @return array
-     */
-    public function readFilterData($excelFile, $column_arr, $startRow = 2, $chunkSize = 600, $sheetIndex = 1)
-    {
-        $excelType = PHPExcel_IOFactory::identify($excelFile);
-        $excelReader = PHPExcel_IOFactory::createReader($excelType);
-        if ($excelType == "CSV") {
-            $excelReader->setInputEncoding('GBK');
-        }
-        $chunkFilter = new ChunkReadFilter($startRow, $chunkSize);
-        $excelReader->setReadFilter($chunkFilter); // 设置实例化的过滤器对象
-        $phpexcel = $excelReader->load($excelFile);
-        $sheetIdx = max((int) $sheetIndex - 1, 0);
-        $activeSheet = $phpexcel->getSheet($sheetIdx);
-
-        $highestColumn = $activeSheet->getHighestColumn(); //最后列数所对应的字母，例如第1行就是A
-        $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
-        if ($highestColumnIndex != count($column_arr)) {
-            throw new Exception('column_arr参数配置错误');
-        }
-        $data = array();
-        $endRow = $startRow + $chunkSize;
-        for ($row = $startRow; $row <= $endRow; $row++) {
-            for ($col = 0; $col < $highestColumnIndex; $col++) {
-                $cell = $activeSheet->getCellByColumnAndRow($col, $row);
-                $value = $cell->getValue();
-                //todo 关于日期判断的部分主要是以下部
-                if ($cell->getDataType() == PHPExcel_Cell_DataType::TYPE_NUMERIC) {
-                    $cellstyleformat = $cell->getParent()->getStyle($cell->getCoordinate())->getNumberFormat();
-                    $formatcode = $cellstyleformat->getFormatCode();
-                    if (preg_match('/^(\[\$[A-Z]*-[0-9A-F]*\])*[hmsdy]/i', $formatcode)) {
-                        $value = gmdate("Y-m-d", PHPExcel_Shared_Date::ExcelToPHP($value));
-                    } else {
-                        $value = PHPExcel_Style_NumberFormat::toFormattedString($value, $formatcode);
-                    }
-                }
-                $data[$row][$column_arr[$col]] = $value;
-            }
-            if (implode('', $data[$row]) == '') {
-                unset($data[$row]);
-            }
-        }
-        return $data;
     }
 
     /**
      * 填充Excel列数据
      * param $list 数据列表
      * param $columns Excel表头数据 $columns = array(
-     *   array('field' => 'name', 'title' => '姓名', 'options' => ['data_type' => 'number', 'format' => '0.000'] ),
+     *   array('field' => 'name', 'title' => '姓名', 'options'=>['type'=>'']),
      *   array('field' => 'list数组元素键值', 'title' => 'B列Excel表头的标题内容'),
-     * );
+     * );.
      */
-    public function pushData(Array $list, Array $columns, $sheet_title)
+    public function pushData(array $list, array $columns, $title, $usePassword = true)
     {
         if (empty($columns) || empty($this->excelObj)) {
             return false;
         }
+
+        set_time_limit(0);
         $objExcel = $this->excelObj;
+
+        /* 设置默认文字居左，上下居中 */
+        $objExcel->getDefaultStyle()->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
+        $objExcel->getDefaultStyle()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
         $objActSheet = $objExcel->getActiveSheet();
+        //默认列宽
+        $objActSheet->getDefaultColumnDimension()->setWidth(15);
+        $objActSheet->getDefaultRowDimension()->setRowHeight(20);
 
         //todo excel表头
         $i = 3;
         foreach ($columns as $key => $column) {
-            $word = PHPExcel_Cell::stringFromColumnIndex($key + 1);
+            $word = Coordinate::stringFromColumnIndex($key + 1);
             $objActSheet->setCellValue($word.$i, $column['title']);
             $objActSheet->getStyle($word.$i)->getFont()->setBold(true);
         }
 
         //todo excel内容
         foreach ($list as $info) {
-            $i++;
+            ++$i;
             foreach ($columns as $key => $column) {
-                $word = PHPExcel_Cell::stringFromColumnIndex($key + 1);
+                $word = Coordinate::stringFromColumnIndex($key + 1);
                 $value = $info[$column['field']];
+
                 if (isset($column['options']['data_type'])) {
                     $dataType = strtoupper($column['options']['data_type']);
-                    $format = isset($column['options']['format']) ? $column['options']['format'] : '';
-                    if ($dataType == 'IMAGE') {
-                        $format = in_array($format, [ 'online', 'local' ]) ? $format : 'local';
-                        if ($format == 'local') { //本地图片
-                            $objDrawing = new PHPExcel_Worksheet_Drawing();
+                    $format = $column['options']['format'];
+                    if ('IMAGE' == $dataType) { //添加图片
+                        $format = in_array($format, ['online', 'local']) ? $format : 'local';
+                        if ('local' == $format) { //本地图片
+                            $objDrawing = new Drawing();
                             if (is_file($value)) {
                                 $objDrawing->setPath($value); //写入图片路径
                             }
-                        } elseif ($format == 'online') { //网络图片
-                            $objDrawing = new PHPExcel_Worksheet_MemoryDrawing();
+                        } elseif ('online' == $format) { //网络图片
+                            $objDrawing = new MemoryDrawing();
                             $objDrawing->setImageResource(imagecreatefrompng($value));
                         } else {
                             continue;
@@ -212,12 +96,12 @@ class Excel
                         //设置表格宽度覆盖默认设置
                         $objActSheet->getColumnDimension($word)->setWidth(100);
                         $objActSheet->getRowDimension($i)->setRowHeight(30);
-                    } elseif ($dataType == 'NUMBER') { //日期，数字，百分比，金额
+                    } elseif ('NUMBER' == $dataType) { //日期，数字，百分比，金额
                         $objActSheet->setCellValue($word.$i, $value);
-                        $format = $format ? $format : PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDDSLASH;
+                        $format = $format ? $format : NumberFormat::FORMAT_DATE_YYYYMMDDSLASH;
                         $objActSheet->getStyle($word.$i)->getNumberFormat()->setFormatCode($format);
-                    } elseif ($dataType == 'STRING') {
-                        $format = $format ? $format : PHPExcel_Cell_DataType::TYPE_STRING;
+                    } elseif ('STRING' == $dataType) {
+                        $format = $format ? $format : DataType::TYPE_STRING;
                         $objActSheet->setCellValueExplicit($word.$i, $value, $format);
                     }
                 } else {
@@ -228,67 +112,107 @@ class Excel
             }
         }
 
+        //设置当前活动sheet的名称和表格标题
         //设置表格边框
-        $style_obj = new PHPExcel_Style();
-        $style_array = array(
-            'borders' => array( //上下左右画线
-                'top' => array( 'style' => PHPExcel_Style_Border::BORDER_THIN ),
-                'left' => array( 'style' => PHPExcel_Style_Border::BORDER_THIN ),
-                'bottom' => array( 'style' => PHPExcel_Style_Border::BORDER_THIN ),
-                'right' => array( 'style' => PHPExcel_Style_Border::BORDER_THIN )
-            )
-        );
-        $style_obj->applyFromArray($style_array);
-        $firstWord = PHPExcel_Cell::stringFromColumnIndex(1);
-        $column_length = count($columns);
-        $maxWord = PHPExcel_Cell::stringFromColumnIndex($column_length);
-        $objActSheet->setSharedStyle($style_obj, "{$firstWord}2:{$maxWord}{$i}");
-
-        //默认列宽
-        $objActSheet->getDefaultColumnDimension()->setWidth(20);
-
-        //设置表格标题
+        $border = [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN, // 设置border样式
+                ],
+            ],
+        ];
+        $firstWord = Coordinate::stringFromColumnIndex(1);
+        $columnLength = count($columns);
+        $maxWord = Coordinate::stringFromColumnIndex($columnLength);
+        $objActSheet->getStyle("{$firstWord}2:{$maxWord}{$i}")->applyFromArray($border);
         $objActSheet->mergeCells("{$firstWord}2:{$maxWord}2");
-        $objActSheet->setCellValue("{$firstWord}2", $sheet_title);
+        $objActSheet->setCellValue("{$firstWord}2", $title);
         $objActSheetStyle = $objActSheet->getStyle("{$firstWord}2");
         $objAlignment = $objActSheetStyle->getAlignment();
-        $objAlignment->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-        $objAlignment->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+        $objAlignment->setVertical(Alignment::VERTICAL_CENTER);
+        $objAlignment->setHorizontal(Alignment::HORIZONTAL_CENTER);
         $objTitleFont = $objActSheetStyle->getFont();
         $objTitleFont->setSize(14);
         $objTitleFont->setBold(true);
 
-        $objActSheet->getProtection()->setPassword(self::PASSWORD);//设置保护密码
-        $objActSheet->getProtection()->setSheet(true);
-        $objActSheet->protectCells("{$firstWord}2:{$maxWord}{$i}");
+        //设置保护密码
+        if ($usePassword) {
+            $objActSheet->getProtection()->setSheet(true);
+            $objActSheet->protectCells("{$firstWord}2:{$maxWord}$i", self::PASSWORD);
+        }
 
         return $i;
     }
-}
 
-/**
- * 读取excel过滤器类 单独文件
- */
-class ChunkReadFilter implements PHPExcel_Reader_IReadFilter
-{
-    private $_startRow = 0; // 开始行
-    private $_endRow = 0; // 结束行
-    private $_columns = []; // 列跨度
-
-    public function __construct($startRow, $chunkSize, $columns = [])
+    /**
+     * 生成Excel文件
+     * param $outputFileName Excel文件名
+     * param $outputExplorer 浏览器输出或文件输出.
+     */
+    public function generatedFile($outputFileName, $outputExplorer = 1)
     {
-        $this->_startRow = $startRow;
-        $this->_endRow = $startRow + $chunkSize;
-        $this->_columns = $columns;
+        $obj = $this->excelObj;
+        $obj->setActiveSheetIndex(0);
+        $objWriter = new Xlsx($obj);
+        if ($outputExplorer > 0) { //输出内容到浏览器
+            header('Content-Type: application/vnd.ms-excel;charset=UTF-8');
+            header("Cache-Control:must-revalidate,post-check=0,pre-check=0");
+            header("Pragma:no-cache");
+            header("Content-Disposition:inline;filename={$outputFileName}");
+            $objWriter->save('php://output');
+            exit;
+        } else { //输出内容到文件通过文件路径再用Ajax无刷新页面
+            $this->_fileName = $outputFileName;
+            $outputFileName = $this->_path.$outputFileName;
+            if (is_file($outputFileName)) {
+                @unlink($outputFileName);
+            }
+            $this->_fileList[] = $outputFileName;
+            $objWriter->save("{$outputFileName}");
+        }
+        $obj->disconnectWorksheets();
+        unset($obj);
     }
 
-    public function readCell($column, $row, $worksheetName = '')
+    /**
+     * 打包文件.
+     *
+     * @param string $fileName
+     *
+     * @return string
+     *
+     * @throws \Exception
+     */
+    public function getZip($fileName = '')
     {
-        if ($row >= $this->_startRow && $row <= $this->_endRow) { //过滤行
-//            if ($this->_columns && in_array($column, $this->_columns)) { //过滤列
-            return true;
-//            }
+        $file = '';
+
+        if (is_array($this->_fileList) && count($this->_fileList) > 0) {
+            if (empty($fileName)) {
+                $fileName = $this->_fileName.'.zip';
+            }
+            $zipName = $this->_path.$fileName;
+            if (is_file($zipName)) { //先删除zip文件重新生成
+                @unlink($zipName);
+            }
+
+            $zip = new \ZipArchive();
+            if (true !== $zip->open($zipName, \ZipArchive::CREATE)) {
+                throw new \Exception('创建压缩压缩文件失败');
+            }
+
+            //加入压缩包
+            foreach ($this->_fileList as $key => $itemFilename) {
+                $zip->addFile($itemFilename, basename($itemFilename));
+            }
+            $zip->close();
+            //删除excel文件
+            foreach ($this->_fileList as $itemFilename) {
+                @unlink($itemFilename);
+            }
+            $file = $zipName;
         }
-        return false;
+
+        return $file;
     }
 }
